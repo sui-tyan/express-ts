3980c102dab59a08290429033c4d05dbe136524d
sui-tyan <mallari23adrian@gmail.com>
2024-03-16 17:39:27 +0800

    fix: can now copy dir

--- index.js
+++ index.js
@@ -24,13 +24,34 @@ const templateDir = path.join(__dirname, 'template');
 fs.promises
   .readdir(templateDir)
   .then((files) =>
-    Promise.all(files.map((file) => copyFile(templateDir, projectDir, file)))
+    Promise.all(
+      files.map((file) => {
+        if (fs.lstatSync(path.join(templateDir, file)).isDirectory()) {
+          copyDir(templateDir, projectDir);
+        } else {
+          copyFile(templateDir, projectDir, file);
+        }
+      })
+    )
   )
-  .then(() => console.log(`Project '${projectName}' created successfully!`))
+  .then(() => {
+    const package_json = path.join(projectDir, 'package.json');
+    fs.readFile(package_json, (err, data) => {
+      const new_package_json = Buffer.from(
+        data.toString().replace('project_name', projectName)
+      );
+      fs.writeFileSync(package_json, new_package_json);
+    });
+    console.log(`Project '${projectName}' created successfully!`);
+  })
   .catch((err) => console.error(err));
 
 function copyFile(sourceDir, destDir, file) {
   const sourcePath = path.join(sourceDir, file);
   const destPath = path.join(destDir, file);
   return fs.promises.copyFile(sourcePath, destPath);
+}
+
+function copyDir(sourceDir, destDir) {
+  return fs.promises.cp(sourceDir, destDir, { recursive: true });
 }
